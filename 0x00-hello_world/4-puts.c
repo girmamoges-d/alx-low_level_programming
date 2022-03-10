43b619af26fc45ac79bf7630bae4302b98558da9
girmamoges-d <girmamoges.d@gmail.com>
2022-03-10 08:33:14 -0800

    Low Level Programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <studio.h>
 int main(void) {
 	/* prints Programming is like building a multilingual puzzle, followed by a new line*/
-	puts("\nProgramming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle")
 	return (0)
 }
