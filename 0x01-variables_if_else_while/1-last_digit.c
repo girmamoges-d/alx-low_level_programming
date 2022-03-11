136d8174096a172f0b43ef77a0038085acb5b4d2
girmamoges-d <girmamoges.d@gmail.com>
2022-03-11 05:06:32 -0800

    Low Level Programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,7 +11,7 @@ int main(void)
 
         srand(time(0));
         n = rand() - RAND_MAX / 2;
-        if (n & 10 > 5 && n % 10 !=0)
+        if (n % 10 > 5 && n % 10 !=0)
         {
                 printf("Last digit of %d is %d and greater than 5\n", n, n % 10);
         }
