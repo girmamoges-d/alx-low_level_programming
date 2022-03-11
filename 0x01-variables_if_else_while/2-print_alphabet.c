1c67755182790b1576053917f603fa5199afbd43
girmamoges-d <girmamoges.d@gmail.com>
2022-03-11 05:18:28 -0800

    Low Level Programming

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Lowercase letters
+*/
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
