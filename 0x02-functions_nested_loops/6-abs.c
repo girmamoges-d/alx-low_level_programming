8c3803f8108c12cd1efd01641d1eb762bd62d9f4
girmamoges-d <girmamoges.d@gmail.com>
2022-03-18 00:59:15 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+*_abs - computes the absolute value of an integer
+*
+*@n: integer with sign
+*
+*Return: 0 Program run succesfully
+**/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (0);
+	}
+}
