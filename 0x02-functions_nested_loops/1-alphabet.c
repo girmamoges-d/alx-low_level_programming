43dcfdd2882af68bced88206f6aa7596a4775cf4
girmamoges-d <girmamoges.d@gmail.com>
2022-03-18 00:00:49 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char lower_case;
+
+	lower_case = 'a';
+
+	while (lower_case<= 'z')
+	{
+		_putchar(lower_case);
+		lower_case++;
+	}
+
+	_putchar('\n');
+}
