636b2ca3170f120d8527cb7d61e0f3b80f4346f0
girmamoges-d <girmamoges.d@gmail.com>
2022-03-18 01:10:07 -0700

    times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+*times_table - prints the 9 times table
+*
+*Return: void
+**/
+
+void times_table(void)
+{
+	int row, col, result;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			result = row * col;
+
+			if ((result / 10) == 0)
+			{
+				if (col == 0)
+				{
+					_putchar('0');
+				}
+				if (col != 0)
+				{
+					_putchar(' ');
+					_putchar((result % 10) + '0');
+				}
+				if (col < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+
