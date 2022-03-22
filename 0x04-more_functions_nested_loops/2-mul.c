0abd0cd21329f92138e854c4b59cd440a9b17fab
girmamoges-d <girmamoges.d@gmail.com>
2022-03-22 06:29:19 -0700

    functions & nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - gives a multiple value
+ * @a: integer value of a
+ * @b: integer value of b
+ * Return: return the result of a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
